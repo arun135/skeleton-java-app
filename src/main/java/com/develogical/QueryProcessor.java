5231595f9afe7dfe2735abbedb624e737bdb0817
Arun Shivanna <arunkumar135@gmail.com>
2020-06-24 17:24:48 +0200

    test changes

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -3,7 +3,11 @@ package com.develogical;
 public class QueryProcessor {
 
     public String process(String query) {
-        if(query.toLowerCase().contains("plus")){
+        if(query.toLowerCase().contains("minus")){
+            int num = findDifference(query);
+            return num+" is the total sum";
+        }
+        else if(query.toLowerCase().contains("plus")){
             int num = getSum(query);
             return num+" is the total sum";
         }
@@ -18,6 +22,24 @@ public class QueryProcessor {
             return "Arunkumr Shivanna";
         }
         return "Not Found";
+    }
+
+    private int findDifference(String query){
+        if (query != null & !query.isEmpty()) {
+            try {
+                int a =
+                        new Integer(query.substring(query.indexOf("is") + 3,
+                                query.indexOf("minus")).trim()).intValue();
+                int b =
+                        new Integer(query.substring(query.indexOf("minus") + 5, query.length()).trim()).intValue();
+
+                return a - b;
+            }catch(Exception e){
+
+            }
+        }
+
+        return -1;
     }
 
     private int findLargest(String query) {
@@ -58,3 +80,5 @@ public class QueryProcessor {
 //        q=2de2b540: which of the following numbers is the largest: 85, 792"
 //        q=3110d8a0: what is 0 plus 12"
 //        q=344c6750: what is 7 plus 9"
+//%20what%20colour%20is%20a%20banana
+//%20what%20is%2019%20minus%2015"
