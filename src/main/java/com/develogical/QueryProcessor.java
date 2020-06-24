ac04843316da763aec8ae4dd4e6c45c74c3d04d7
Arun Shivanna <arunkumar135@gmail.com>
2020-06-24 16:14:42 +0200

    test changes

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -3,8 +3,8 @@ package com.develogical;
 public class QueryProcessor {
 
     public String process(String query) {
-        if (query.toLowerCase().contains("romeo and juliet")) {
-            return "William Shakespeare.";
+        if (query.toLowerCase().contains("what is your name")) {
+            return "Arunkumr Shivanna";
         }
         return "Not Found";
     }
